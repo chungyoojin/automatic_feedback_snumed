6b3543517982c80f379d1e78164f8e50d19dbb8d
chungyoojin <134499664+chungyoojin@users.noreply.github.com>
2023-06-04 22:46:38 +0900

    Update 1_문항설계(1).py

--- pages/1_문항설계(1).py
+++ pages/1_문항설계(1).py
@@ -1,5 +1,13 @@
 import streamlit as st
 
 st.set_page_config(layout="wide")
-st.title("문항 설계의 전반적인 원리")
+st.title("✨문항 설계의 전반적인 원리")
+st.subtitle("문항 설계 목표")
+st.write("1. 식의 계산 단원에서 학생들이 가지고 있는 인지 요소 분석")
+st.write("2. 식의 계산 단원에서 학생들이 가지고 있는 오개념 및 오류 분석")
+
+st.subtitle("식의 계산 단원 내 지식맵 구성")
+
+
+
 st.write("여기 누군가 적어주세요ㅠㅠ")
